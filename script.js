df95a86c1fbbf97a988889d52ade45c9b38526de
Silvia-code-hub <silviamunyoroku@gmail.com>
2025-04-02 10:53:46 +0300

    script.js

---
+++ script.js
@@ -0,0 +1,49 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert("You must write something!");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+        inputBox.value = "";
+        saveData();
+    }
+}
+
+function editTask(task) {
+    const newTask = prompt("Edit your task:", task.innerHTML);
+    if (newTask) {
+        task.innerHTML = newTask;
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        task.appendChild(span);
+        saveData();
+    }
+}
+
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        editTask(e.target);
+        saveData();
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
